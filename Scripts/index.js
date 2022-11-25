eb554b865980d0f3609042f2aeb83b89163decfd
jongin12 <jongin981224@gmail.com>
2022-11-25 16:01:00 +0900

    몬스터 추가

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,9 +1,4 @@
-import { makeUnit, moveUnit, attackUnit } from "./unit.js";
-const styling = (target, style) => {
-  for (let i in style) {
-    target.style[i] = style[i];
-  }
-};
+import { styling, gameStart } from "./unit.js";
 
 const root = document.getElementById("root");
 styling(root, {
@@ -15,32 +10,38 @@ const canvas = document.getElementById("canvas");
 styling(canvas, {
   width: "600px",
   height: "800px",
-  backgroundColor: "#888",
+  backgroundColor: "skyblue",
   overflow: "hidden",
 });
 
 let move_switch = false;
+function moving(unit, speed, direction, clear) {
+  if (!move_switch) {
+    move_switch = true;
+    unit.moveUnit(speed, direction, clear).then(() => {
+      move_switch = false;
+    });
+  }
+}
+
 document.addEventListener("keydown", (e) => {
   if (e.key === "a") {
-    if (!move_switch) {
-      move_switch = true;
-      moveUnit(user, -1, "left", 5).then(() => {
-        move_switch = false;
-      });
-    }
+    moving(user, -1, "left", 15);
   } else if (e.key === "d") {
-    if (!move_switch) {
-      move_switch = true;
-      moveUnit(user, 1, "left", 5).then(() => {
-        move_switch = false;
-      });
-    }
-    move_switch = true;
+    moving(user, 1, "left", 15);
+  } else if (e.key === "s") {
+    moving(user, 1, "top", 15);
+  } else if (e.key === "w") {
+    moving(user, -1, "top", 15);
+  } else if (e.key === "q") {
+    user.att = 100;
+  } else if (e.key === "e") {
+    user.att = 30;
   }
 });
 
 class Unit {
-  constructor(name, hp, att, attSpeed, speed, top, left, width, height) {
+  constructor(name, hp, att, attSpeed, speed, top, left, width, height, img) {
     this.name = name;
     this.hp = hp;
     this.att = att;
@@ -50,14 +51,139 @@ class Unit {
     this.left = left;
     this.width = width;
     this.height = height;
+    this.img = img;
+  }
+  makeUnit() {
+    const div = document.createElement("div");
+    div.className = this.name;
+    styling(div, {
+      width: this.width + "px",
+      height: this.height + "px",
+      position: "absolute",
+      top: this.top + "px",
+      left: this.left + "px",
+    });
+    canvas.appendChild(div);
+    const img = document.createElement("img");
+    img.src = this.img;
+    img.style.width = this.width + "px";
+    img.style.height = this.height + "px";
+    div.appendChild(img);
+    const hpBar = document.createElement("div");
+    hpBar.style.width = "80%";
+    hpBar.style.height = "10%";
+    hpBar.style.backgroundColor = "white";
+    hpBar.style.position = "absolute";
+    hpBar.style.left = "10%";
+    hpBar.style.top = "100%";
+    div.appendChild(hpBar);
+    const nowHp = document.createElement("div");
+    nowHp.style.width = "100%";
+    nowHp.style.height = "100%";
+    nowHp.style.backgroundColor = "red";
+    hpBar.appendChild(nowHp);
+    let maxHp = this.hp;
+    let hpInterval = setInterval(() => {
+      nowHp.style.width = (this.hp / maxHp) * 100 + "%";
+      if (this.hp <= 0) {
+        clearInterval(hpInterval);
+      }
+    }, 1000 / 60);
+  }
+  moveUnit(speed, direction, clear) {
+    return new Promise((resolve, reject) => {
+      if (speed > 0 && this.left >= 600 - this.width) {
+        resolve();
+      } else if (speed < 0 && this.left <= 0) {
+        resolve();
+      } else if (speed > 0 && this.top >= 800 - this.height) {
+        resolve();
+      } else if (speed < 0 && this.top <= 0) {
+        resolve();
+      } else {
+        let unitClass = document.getElementsByClassName(this.name);
+        let unitClass0 = unitClass[0];
+        let now = this[direction];
+        let obj = {};
+        obj[direction] = setInterval(() => {
+          this[direction] += (this.speed * speed) / 60;
+          unitClass0.style[direction] = this[direction] + "px";
+          if (this[direction] <= 0) {
+            clearInterval(obj[direction]);
+            resolve();
+          }
+          if (speed > 0 && this[direction] >= now + clear) {
+            clearInterval(obj[direction]);
+            resolve();
+          }
+          if (speed < 0 && this[direction] <= now - clear) {
+            clearInterval(obj[direction]);
+            resolve();
+          }
+        }, 1000 / 60);
+      }
+    });
   }
 }
 
-const user = new Unit("me", 100, 10, 10, 300, 650, 250, 80, 80);
-const monster = new Unit("monster", 100, 100, 10, 100, 0, 50, 50, 50);
-makeUnit(user, "blue");
-attackUnit(user, monster);
+const user = new Unit(
+  "me", //name
+  100, //hp
+  30, //att
+  3, //attspeed
+  300, //speed
+  650, //top
+  250, //left
+  80, //width
+  80, //height
+  "./img/aaaa.png" //img
+);
+const monster1 = new Unit(
+  "monster1", //name
+  100, //hp
+  30, //att
+  0, //attspeed
+  40, //speed
+  0, //top
+  50, //left
+  50, //width
+  50, //height
+  "./img/bbb.png" //img
+);
 
-makeUnit(monster, "red");
-moveUnit(monster, 1, "top", 500);
-moveUnit(monster, 1, "left", 300);
+const monster2 = new Unit(
+  "monster2", //name
+  100, //hp
+  30, //att
+  0, //attspeed
+  40, //speed
+  0, //top
+  450, //left
+  50, //width
+  50, //height
+  "./img/bbb.png" //img
+);
+
+const randomMonster = new Unit(
+  "random", //name
+  Math.floor(Math.random() * 100 + 100), //hp
+  Math.floor(Math.random() * 20 + 20), //att
+  0, //attspeed
+  Math.floor(Math.random() * 100 + 40), //speed
+  0, //top
+  200, //left
+  50, //width
+  50, //height
+  "./img/bbb.png" //img
+);
+console.log(randomMonster);
+
+user.makeUnit();
+monster1.makeUnit();
+monster2.makeUnit();
+randomMonster.makeUnit();
+monster1.moveUnit(1, "top", 700);
+monster2.moveUnit(1, "top", 700);
+randomMonster.moveUnit(1, "top", 700);
+gameStart(user, [monster1, monster2, randomMonster]);
+// moveUnit(monster, 1, "left", 300);
--- Scripts/unit.js
+++ Scripts/unit.js
@@ -1,17 +1,22 @@
-export function makeUnit(unit, color) {
-  const div = document.createElement("div");
-  div.className = unit.name;
-  div.style.width = unit.width + "px";
-  div.style.height = unit.height + "px";
+export const styling = (target, style) => {
+  for (let i in style) {
+    target.style[i] = style[i];
+  }
+};
 
-  div.style.position = "absolute";
-  div.style.top = unit.top + "px";
-  div.style.left = unit.left + "px";
-
-  div.style.backgroundColor = color;
-  canvas.appendChild(div);
-  const div2 = document.createElement("div");
-  div.appendChild(div2);
+class Unit {
+  constructor(name, hp, att, attSpeed, speed, top, left, width, height, img) {
+    this.name = name;
+    this.hp = hp;
+    this.att = att;
+    this.attSpeed = attSpeed;
+    this.speed = speed;
+    this.top = top;
+    this.left = left;
+    this.width = width;
+    this.height = height;
+    this.img = img;
+  }
 }
 
 export function deleteUnit(unit) {
@@ -20,73 +25,83 @@ export function deleteUnit(unit) {
   unitClass0.remove();
 }
 
-export function moveUnit(unit, speed, direction, clear) {
-  return new Promise((resolve, reject) => {
-    if (speed > 0 && unit.left >= 600 - unit.width) {
-      resolve();
-    } else if (speed < 0 && unit.left <= 0) {
-      resolve();
-    } else if (speed > 0 && unit.top >= 800 - unit.height) {
-      resolve();
-    } else if (speed < 0 && unit.top <= 0) {
-      resolve();
-    } else {
-      let unitClass = document.getElementsByClassName(unit.name);
-      let unitClass0 = unitClass[0];
-      let now = unit[direction];
-      let obj = {};
-      obj[direction] = setInterval(() => {
-        unit[direction] += (unit.speed * speed) / 60;
-        unitClass0.style[direction] = unit[direction] + "px";
-        if (unit[direction] <= 0) {
-          clearInterval(obj[direction]);
-          resolve();
-        }
-        if (speed > 0 && unit[direction] >= now + clear) {
-          clearInterval(obj[direction]);
-          resolve();
-        }
-        if (speed < 0 && unit[direction] <= now - clear) {
-          clearInterval(obj[direction]);
-          resolve();
-        }
-      }, 1000 / 60);
-    }
-  });
-} //unit이 초당 speed만큼 direction방향으로 이동, clear만큼 이동후 종료
+// export function moveUnit(unit, speed, direction, clear) {
+//   return new Promise((resolve, reject) => {
+//     if (speed > 0 && unit.left >= 600 - unit.width) {
+//       resolve();
+//     } else if (speed < 0 && unit.left <= 0) {
+//       resolve();
+//     } else if (speed > 0 && unit.top >= 800 - unit.height) {
+//       resolve();
+//     } else if (speed < 0 && unit.top <= 0) {
+//       resolve();
+//     } else {
+//       let unitClass = document.getElementsByClassName(unit.name);
+//       let unitClass0 = unitClass[0];
+//       let now = unit[direction];
+//       let obj = {};
+//       obj[direction] = setInterval(() => {
+//         unit[direction] += (unit.speed * speed) / 60;
+//         unitClass0.style[direction] = unit[direction] + "px";
+//         if (unit[direction] <= 0) {
+//           clearInterval(obj[direction]);
+//           resolve();
+//         }
+//         if (speed > 0 && unit[direction] >= now + clear) {
+//           clearInterval(obj[direction]);
+//           resolve();
+//         }
+//         if (speed < 0 && unit[direction] <= now - clear) {
+//           clearInterval(obj[direction]);
+//           resolve();
+//         }
+//       }, 1000 / 60);
+//     }
+//   });
+// } //unit이 초당 speed만큼 direction방향으로 이동, clear만큼 이동후 종료
 
-export function attackUnit(unit, monster) {
-  let makeDiv = setInterval(() => {
-    let div = document.createElement("div");
-    div.style.backgroundColor = "white";
-    div.style.width = "4px";
-    div.style.height = "15px";
-    div.style.position = "absolute";
-    let top = unit.top - 15;
-    div.style.top = top + "px";
-    let left = unit.left + unit.width / 2;
-    div.style.left = left + "px";
-    canvas.appendChild(div);
-    let attack = setInterval(() => {
-      top -= 10;
+export function gameStart(unit, monster) {
+  let monsterCount = monster.length;
+  return new Promise((resolve, reject) => {
+    let makeDiv = setInterval(() => {
+      let div = document.createElement("div");
+      div.style.backgroundColor = "white";
+      div.style.width = "4px";
+      div.style.height = "10px";
+      div.style.position = "absolute";
+      let top = unit.top - 10;
       div.style.top = top + "px";
-      if (monster) {
-        if (monster.left < left && monster.left + monster.width > left) {
-          if (monster.top + monster.height >= top) {
-            clearInterval(attack);
-            div.remove();
-            monster.hp -= unit.att;
-            console.log(monster.hp);
-            if (monster.hp <= 0) {
-              deleteUnit(monster);
-              monster = null;
+      let left = unit.left + unit.width / 2;
+      div.style.left = left + "px";
+      canvas.appendChild(div);
+      let attack = setInterval(() => {
+        top -= 10;
+        div.style.top = top + "px";
+        monster.map((item, index) => {
+          if (item) {
+            if (item.left < left && item.left + item.width > left) {
+              if (item.top + item.height >= top && item.top <= top) {
+                clearInterval(attack); //총알 이동멈춤
+                div.remove(); //총알 삭제
+                item.hp -= unit.att; //데미지
+                if (item.hp <= 0) {
+                  deleteUnit(item);
+                  monster.splice(index, 1);
+                  monsterCount--;
+                  if (monsterCount <= 0) {
+                    clearInterval(makeDiv); //총알쏘는거 멈춤
+                    resolve(index);
+                  }
+                }
+              }
             }
           }
+        });
+        if (top < -10) {
+          clearInterval(attack);
+          div.remove();
         }
-      } else if (top < -15) {
-        clearInterval(attack);
-        div.remove();
-      }
-    }, 1000 / 60);
-  }, 300);
+      }, 1000 / 60);
+    }, 1000 / unit.attSpeed);
+  });
 }
