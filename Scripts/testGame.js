4a4939b2a9653795a85c85d65160d07c0c252e6c
jongin12 <jongin981224@gmail.com>
2022-12-29 17:24:37 +0900

    속도설정기능 추가

--- Scripts/testGame.js
+++ Scripts/testGame.js
@@ -41,6 +41,37 @@ styling(line, {
 });
 canvas.appendChild(line);
 
+const setting = document.createElement("div");
+styling(setting, {
+  width: "100%",
+  height: "100%",
+  backgroundColor: "white",
+  position: "absolute",
+  top: "0px",
+});
+canvas.appendChild(setting);
+
+const speed = document.createElement("input");
+styling(speed, {
+  width: "100px",
+  height: "20px",
+  border: "1px solid black",
+});
+setting.appendChild(speed);
+
+const button = document.createElement("button");
+styling(button, {
+  width: "100px",
+  height: "20px",
+  border: "1px solid black",
+});
+setting.appendChild(button);
+
+button.addEventListener("click", () => {
+  setting.remove();
+  play(testDataArr.reverse(), 0.4, Number(speed.value));
+});
+
 let lineArr = canvas.children;
 function createNote(data, speed) {
   data.map((item, index) => {
@@ -58,17 +89,18 @@ function createNote(data, speed) {
         note.style.top = `${top}px`;
         top += speed;
         if (keySwitch[index]) {
-          if (top > 680 && top < 700) {
+          if (top > 670 && top < 710) {
             console.log("perfect");
             clearInterval(moveNote);
             note.remove();
-          } else if (top > 670 && top < 710) {
+          } else if (top > 650 && top < 730) {
             console.log("good");
             clearInterval(moveNote);
             note.remove();
           }
         }
-        if (top > 800) {
+        if (top > 780) {
+          console.log("bad");
           clearInterval(moveNote);
           note.remove();
         }
@@ -80,12 +112,36 @@ function createNote(data, speed) {
 // const testData = [1, 0, 0, 0, 0, 0, 0, 1];
 // createNote(testData, 1);
 
-const testDataArr = [
-  [1, 0, 0, 0, 0, 0, 0, 1],
-  [0, 1, 0, 0, 0, 0, 1, 0],
-  [0, 0, 1, 0, 0, 1, 0, 0],
-  [0, 0, 0, 1, 1, 0, 0, 0],
+const testArr = [
+  "10001000",
+  "10000000",
+  "10000010",
+  "10000000",
+  "10001000",
+  "10000000",
+  "10000010",
+  "10000000",
+  "10001000",
+  "10000000",
+  "10000010",
+  "00000000",
+  "00001000",
+  "00010000",
+  "00000100",
+  "00100000",
+  "00000010",
+  "01000000",
+  "00000001",
+  "10000000",
 ];
+
+const testDataArr = testArr.map((item) => {
+  let arr = [];
+  for (let i = 0; i < 8; i++) {
+    arr.push(Number(item[i]));
+  }
+  return arr;
+});
 
 function play(dataArr, delay, speed) {
   let index = 0;
@@ -97,7 +153,6 @@ function play(dataArr, delay, speed) {
     }
   }, delay * 1000);
 }
-play(testDataArr.reverse(), 0.5, 3);
 
 const keySwitch = [0, 0, 0, 0, 0, 0, 0, 0];
 const keyData = {
@@ -113,12 +168,23 @@ const keyData = {
 
 document.addEventListener("keydown", (e) => {
   let index = keyData[e.key];
-  if (!keySwitch[index]) {
-    lineArr[index].style.backgroundColor = "rgba(0,0,0,0.2)";
-    keySwitch[index] = 1;
-    setTimeout(() => {
-      lineArr[index].style.backgroundColor = "rgba(0,0,0,0)";
-      keySwitch[index] = 0;
-    }, 50);
+  if (
+    e.key === "a" ||
+    e.key === "s" ||
+    e.key === "d" ||
+    e.key === "f" ||
+    e.key === "j" ||
+    e.key === "k" ||
+    e.key === "l" ||
+    e.key === ";"
+  ) {
+    if (!keySwitch[index]) {
+      lineArr[index].style.backgroundColor = "rgba(0,0,0,0.2)";
+      keySwitch[index] = 1;
+      setTimeout(() => {
+        lineArr[index].style.backgroundColor = "rgba(0,0,0,0)";
+        keySwitch[index] = 0;
+      }, 50);
+    }
   }
 });
