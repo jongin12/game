2823feb0abf821177f2be5ff2c2012d487db8686
jongin12 <jongin981224@gmail.com>
2022-12-29 17:45:37 +0900

    속도랑 개수 설정 추가

--- Scripts/testGame.js
+++ Scripts/testGame.js
@@ -41,36 +41,59 @@ styling(line, {
 });
 canvas.appendChild(line);
 
-const setting = document.createElement("div");
-styling(setting, {
-  width: "100%",
-  height: "100%",
-  backgroundColor: "white",
-  position: "absolute",
-  top: "0px",
-});
-canvas.appendChild(setting);
+{
+  const setting = document.createElement("div");
+  styling(setting, {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "white",
+    position: "absolute",
+    top: "0px",
+  });
+  canvas.appendChild(setting);
 
-const speed = document.createElement("input");
-styling(speed, {
-  width: "100px",
-  height: "20px",
-  border: "1px solid black",
-});
-setting.appendChild(speed);
+  const speed = document.createElement("input");
+  styling(speed, {
+    width: "100px",
+    height: "20px",
+    border: "1px solid black",
+  });
+  speed.placeholder = "속도";
+  setting.appendChild(speed);
 
-const button = document.createElement("button");
-styling(button, {
-  width: "100px",
-  height: "20px",
-  border: "1px solid black",
-});
-setting.appendChild(button);
+  const noteNum = document.createElement("input");
+  styling(noteNum, {
+    width: "100px",
+    height: "20px",
+    border: "1px solid black",
+  });
+  noteNum.placeholder = "노트 개수";
+  setting.appendChild(noteNum);
 
-button.addEventListener("click", () => {
-  setting.remove();
-  play(testDataArr.reverse(), 0.4, Number(speed.value));
-});
+  const button = document.createElement("button");
+  styling(button, {
+    width: "100px",
+    height: "20px",
+    border: "1px solid black",
+  });
+  button.textContent = "시작";
+  setting.appendChild(button);
+
+  const text = document.createElement("p");
+  styling(text, {
+    width: "100%",
+    height: "100px",
+  });
+  text.textContent = "둘다 숫자만 입력";
+  setting.appendChild(text);
+
+  button.addEventListener("click", () => {
+    let speedValue = Number(speed.value);
+    let NoteValue = Number(noteNum.value);
+    setting.remove();
+    play(randomStage(NoteValue).reverse(), 1 / speedValue, speedValue);
+  });
+}
 
 let lineArr = canvas.children;
 function createNote(data, speed) {
@@ -109,9 +132,6 @@ function createNote(data, speed) {
   });
 }
 
-// const testData = [1, 0, 0, 0, 0, 0, 0, 1];
-// createNote(testData, 1);
-
 const testArr = [
   "10001000",
   "10000000",
@@ -135,6 +155,25 @@ const testArr = [
   "10000000",
 ];
 
+const stage1 = [
+  "00100000",
+  "00001000",
+  "00000000",
+  "00000001",
+  "10000000",
+  "00000100",
+  "00100000",
+  "00000010",
+  "00001000",
+  "00001000",
+  "00100000",
+  "00100000",
+  "10000000",
+  "10000000",
+  "00000001",
+  "00000001",
+];
+
 const testDataArr = testArr.map((item) => {
   let arr = [];
   for (let i = 0; i < 8; i++) {
@@ -142,6 +181,23 @@ const testDataArr = testArr.map((item) => {
   }
   return arr;
 });
+const stage1Arr = stage1.map((item) => {
+  let arr = [];
+  for (let i = 0; i < 8; i++) {
+    arr.push(Number(item[i]));
+  }
+  return arr;
+});
+const randomStage = (number) => {
+  let arr = [];
+  for (let i = 0; i < number; i++) {
+    let arrr = [0, 0, 0, 0, 0, 0, 0, 0];
+    let random = Math.floor(Math.random() * 8);
+    arrr[random] = 1;
+    arr.push(arrr);
+  }
+  return arr;
+};
 
 function play(dataArr, delay, speed) {
   let index = 0;
